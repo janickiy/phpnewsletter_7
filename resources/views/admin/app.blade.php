c2ad6955528772db805a3815fe211e7fe76d01e4
janickkiy <janickiy@mail.ru>
2024-09-17 18:45:06 +0300

    bugfix

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -90,9 +90,8 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="{{ URL::route('admin.templates.index') }}" class="brand-link">
-            <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-                 style="opacity: .8">
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
+            <img src="../../dist/img/logo.png" alt="AdminLTE Logo" class="brand-image " style="">
+            <span class="">{{ env('VERSION') }}</span>
         </a>
 
         <!-- Sidebar -->
@@ -320,9 +319,6 @@
     <!-- /.content-wrapper -->
 
     <footer class="main-footer">
-        <div class="float-right d-none d-sm-block">
-            <b>{{ env('VERSION') }}</b>
-        </div>
         <strong>&copy; 2006-{{ date('Y') }} <a href="https://janicky.com">PHP Newsletter</a>, {{ trans('frontend.str.author') }}</strong>
     </footer>
 
