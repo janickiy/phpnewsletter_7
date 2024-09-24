9ba553478636a0842e3b2f307281438006db5efc
janickkiy <janickiy@mail.ru>
2024-09-24 19:33:26 +0300

    bugfix

--- resources/lang/en/license.php
+++ resources/lang/en/license.php
@@ -3,61 +3,110 @@
 return [
     'agreement' => "Software Product License Agreement
 
-The present License Agreement is deemed to be the plainly required agreement, concluded between You (a natural or a legal person) (hereinafter referred to as User), and Alexander Yanitsky is the owner of the exclusive rights to the Program (\"Author\"),
-for using the program PHP Newsletter by the User (hereinafter referred to as Products), written by the Author and distributed on a commercial basis.
-The present License Agreement covers the Product updates and add-ins, facilitating its execution. All the rights to the Product belong to the Author.
-The exclusive right to the Product distribution belongs to the Author. The whole Product or any part thereof (including but not limited to data bases, graphical images, photos, videos, texts, and add-in programs) are deemed to be the separate copyright items.
-By installing, copying, downloading, providing access to, or by any other way utilizing the stated Software Product, You accept the terms of the present License agreement in doing so. In case You do not yield to any of the present License Agreement’s terms, do not install or use the present Product.
+CONDITIONS FOR MAKING COPIES, DISTRIBUTION AND MODIFICATIONS
 
-License Agreement:
+This License Agreement applies to all software or other developed by JANICKY.COM, in which it is
+indicated that the program may be distributed only under this license agreement.
+The term \"software product\" , as used below, is applied to a program (development) and the term
+\"development based on the Software product\" refers to either the software product, or any
+development made on its basis, protected by copyright law.
+This license agreement is addressed to you and enters into agreement with you.
+This license agreement applies only to manufacturing copies, distribution and modification of the
+software product. Other actions on Software product are beyond the scope of this license agreement.
 
-The Product (including all its integral parts) is licensed, NOT SOLD, to You for use under the terms of the present License Agreement.
+1. It is permitted to copy and distribute replicas of the source code of Software product in the form in
+which you received them, in any medium provided that each copy you supply with references to
+copyright law and disclaimer of warranty; keep intact everything relating to this license agreement and
+the absence of any warranty, and give the Software product to any third-party beneficiaries together
+with a copy of the Software Product License agreement along with the Program.
 
-1. SUBJECT OF AGREEMENT
+2. You can make changes to your copy of the Software Product in any part of it, thus creating a
+development on the basis of Software product, and copy and distribute such modifications or
+developments under the conditions listed above in section 1, in case they also meet the following
+conditions:
 
-1.1. The Subject of the present License Agreement is deemed to be the Product license transfer for use to You, as the User, by the Author, as the Right Holder, i.e. transfer of the right to use the Product under the terms and in the way listed below.
-1.2. All the terms, hereafter specified, refer both to the Product as the whole and to all its components separately.
+2.1 you must specify exactly which files have been changed, what was changed, and date of changes.
 
-2. PROPERTY RIGHTS
+2.2 Any distributed or published developments that include the Software product entirely or any part of
+it, made on the basis of the software product, or any part of it, must be mandatory licensed by you
+according to the conditions of the present license agreement.
 
-2.1. All the Product rights (including but not limited to the rights to any data bases, graphical images, photos, videos, audios, texts, add-in programs, and other copyright objects, integrated into the Product), accompanying materials and any Product copies belong to the Author.
-2.2. The User is granted an absolute License to use the Product for personal commercial or non-commercial purposes, explicitly in the ways, listed below, subject to the requirements of the terms, stated below.
-2.3. The Author does not grant You any rights, except for the rights, expressly declared in the present License Agreement.
+3. If a modified program is designed for interactive use, then at the beginning of the work with such
+interactive use, the information has to be shown in a normal procedure about the presence and the
+ability to view copyright notices and the absence (or presence provided by you) of guarantees that users
+can according to this agreement transfer software product to other users, as well as instructions for the
+user for how to view a copy of this license agreement.
+These requirements apply to the modified development in general.
+If logically isolated components of your design are not based on this Software product and can rightfully
+be regarded as independent development, then this License and its conditions do not apply to the
+components mentioned in those cases when you distribute them as separate development.
+If these components are distributed as part of an integrated development based on the Software
+product, then this license agreement is effective, permitting the licensing of this integral development,
+and therefore, applicable to all and any of its parts, regardless of who is its author.
 
-3. TERMS OF USE
+4. It is possible to make copies, modify, sublicense or distribute the Software product only subject to
+the conditions expressly stated in this license agreement. Any attempt otherwise to copy, modify,
+sublicense or distribute the Software product are considered a violation of the license agreement and
+will automatically terminate your rights provided by this agreement.
+However, a license of third parties who received from you any copies or rights under this license
+agreement will remain valid as long as such parties remain fully comply with the conditions of the
+agreement.
 
-3.1. The User may use the Product in any required way and for any purpose, unless it violates the terms of the present Agreement.
-3.2. The User may not transfer the Product in any form to the third parties or organizations, no matter what the purpose of such transfer is.
-3.3. The Product, you purchased, may be used on one targeted domain only, except for the license for an unlimited number of domains.
-3.4. In order the Product is installed on the other domain, You shall acquire the Software Product again, having specified the new targeted domain, except for the license for an unlimited number of domains.
-3.5. The removal of information about the Author out of the Software Product is prohibited.
-3.6. The Product may be used for the User’s private purposes only, provided, that the Product or any part of it may not be transferred to the third parties, that have not obtained the Product from the Author directly, and in no other way.
-3.7. The User may request the technical assistance from the Author to restore the lost or damaged exemplar of the Product. Herewith, the User shall provide the confirmation of the lawful use of the Product’s copy.
-3.8. The User shall neither perform independently nor enable the other natural or legal persons to perform the following acts:
-- use the Product for the direct generation of profit through its distribution in any form;
-- lease, rent out or provide the Product for temporary use to the third parties;
-- establish conditions for the Product’s use by the people, not personally entitled to use it (including the people working in the same net or multiuser system as You do), retransfer the Software Product License;
-- release (publish) any fragments or integral parts of the Product, as well as to offer an opportunity to other parties to copy it;
-- transfer the Software Product usage rights to the third parties;
-- perform any activity with regard to the Software Product, violating the applicable Russian Federation laws and other legal acts, as well as the statutes of international law.
-3.9. In case the User performs any of the acts, listed in Cl. 3.8, the Author, in its sole discretion, shall reserve the right to terminate the present Agreement at any time.
+5. You are not required to accept this License Agreement, since you have not signed it. However,
+nothing else grants you a permission to modify or distribute the Software product or create any
+developments on its basis. In this case, such actions are prohibited by law if you do not accept this
+License agreement.
+Therefore, by modifying or distributing a copy of the Software product (or any work created based on
+it), you thus show the acceptance of this License with all its terms and conditions for copying,
+distributing or modifying the Software product or developments created on its basis.
 
-4. UPDATES
+6. Whenever you redistribute the Software Product (or development created based on the Product), the
+new recipient automatically receives from the original licensor a right to copy, distribute or modify the
+Software Product in accordance with the terms and conditions of this license agreement. You can not
+impose any further restrictions on the rights of the new owner given to him by this license agreement.
+You are not responsible for enforcing compliance by third parties to this agreement.
 
-4.1. In order one uses the software, referred by the Author to as the Updates, one shall obtain the Product license. The Update completely or partly substitutes or supplements the Product, being Your title to obtain the Update.
-After the Update is installed, You may use it in accordance with the terms of the present Agreement only.
-4.2. The Author shall not be explicitly or implicitly liable to create or distribute the Updates. The Updates are released whenever possible and necessary. The Author shall not be liable to inform the Users on the Update release.
-The information on the Updates released is published on the Author’s website.
-4.3. The Author reserves the right to announce both free and paid Product Updates or its integral parts.
+7. In case of a court judgment or a copy infringement or for any other reason (not necessarily of patent
+character), you (whether by court order, agreement or otherwise) are put in conditions that contradict
+the conditions of this license agreements, this does not excuse you from the conditions of this license
+agreement.
+If you can not simultaneously perform your obligations under this License and any other pertinent
+obligations, then as consequence you can not distribute the Software Product.
+For example, if a patent license would not allow gratuitous further distribution of software products by
+those who received a copy of it from you, directly or indirectly, then the only way not to violate any of
+its conditions or conditions of this license agreement is to fully refrain entirely from distribution of the
+Software Product.
+If any part of this section is held to have no legal force or inapplicable in the circumstances, the rest part
+of the section is intended to apply and the section as a whole is applicable to other circumstances.
 
-5. LIABILITY
+8. If the distribution and/or use the Software Product in a specific country is limited by patent
+agreements or relationships in the field of copyright, the original copyright holder who places his
+software product under this License Agreement, can add a clearly defined geographical limitations for a
+distribution, excluding these countries from those that allowed distribution. In such case, this License
+agreement incorporates the limitation as if written in the actual text agreement.
 
-5.1. In case the present Agreement is violated, the User forfeits the License to use the Product.
+9. If you wish to incorporate parts of the Software product into other freely distributed software
+products, but under other conditions, you have to ask a permission in writing from the authors.
+DISCLAIMER OF WARRANTY
 
-6. LIMITED WARRANTY
+10 . BECAUSE THE SOFTWARE IS LICENSED FREE OF CHARGE, NO WARRANTIES ARE GIVEN TO THE
+EXTENT ALLOWED BY LAW. EXCEPT WHEN THERE IS WRITTEN STATEMENT OF HOLDERS OR
+DISTRIBUTORS OF SOFTWARE, THE PRODUCT IS PROVIDED TO YOU \"AS IT IS\", THAT DOES NOT PROVIDE
+ANY WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF FITNESS FOR SALES, AND USE FOR A
+PARTICULAR PURPOSE. YOU ASSUME THE ENTIRE RISK AS TO THE ELIGIBILITY REQUIREMENTS FOR
+QUALITY AND PERFORMANCE OF THE SOFTWARE. IF THE SOFTWARE WORKS INCORRECTLY, YOU
+SOLELY BARE ALL NECESSARY COSTS FOR SERVICE, TROUBLESHOOTING AND CORRECTION.
 
-6.1. The Author guarantees the principal working capacity of the Product on the appropriate software and hardware platforms.
-6.2. The Software Product is distributed “as-is”. The Author shall not be liable for the Product not to meet the User’s expectations and perceptions. The Author does not guarantee, that the Product is free of bugs. The Author shall not be liable for the direct or indirect consequences of Product application, including those, caused by the possible bugs or misprints.
-6.3. The Author shall remove the Product bugs, brought to light by the User, for free.
-6.4. The acquired Product is nonreturnable on any consideration without the Author’s consent.",
+11. UNDER NO CIRCUMSTANCES, EXCEPT WHEN REQUIRED BY APPLICABLE LAW OR AGREED TO IN
+WRITING, NO COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO AMEND AND/OR REDISTRIBUTE THE
+SOFTWARE PRODUCT AS PERMITTED ABOVE, IS LIABLE TO YOU FOR DAMAGES, INCLUDING GENERAL,
+SPECIAL , INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF USE OR INABILITY TO USE THE
+SOFTWARE (INCLUDING LOSS OF DATA, INCONVINIENT FOR YOU METHOD OF DATA RECEPTION OR ANY
+ADDITIONAL EXPENSES SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE TO OPERATE WITH ANY
+OTHER SOFTWARE, EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
+Distributing this program, we hope that it will be useful, but DO NOT GIVE ANY WARRANTY, without
+even the implied warranty of MERCHANTIBILITY OR USAGE FOR A PARTICULAR PURPOSE. Together with
+this program you need to get our license for the software
+"
 ];
